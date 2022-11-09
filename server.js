c8499d4aff4cc66fba4aeb697cbf1bc24e16551e
= <=>
2022-11-09 11:29:05 -0300

    Atualização

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var port = process.env.PORT || 8080;
+
 const express = require('express');
 const path = require('path');
 
@@ -5,7 +7,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(process.env.PORT || 8080, (err) => {
+app.listen(8080, (err) => {
 	err ? console.log("err", err) : console.log ("Tudo funcionando!")
 });
 
